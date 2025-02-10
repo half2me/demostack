a42ad4dab6d237f8e915d8900459b2c18b0532e4
Benjamin Tamasi <halftome@gmail.com>
2025-02-10 20:27:40 +0100

    migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,21 @@
-import { defineConfig } from 'drizzle-kit'
+import { type Config, defineConfig } from 'drizzle-kit'
+
+// Some hacky configuration needed to be able to use the local D1 instance that miniflare provides
+
+const localConfig: Partial<Config> = {
+	dbCredentials: {
+		url: process.env.LOCAL_DB_PATH!,
+	},
+}
+
+const remoteConfig: Partial<Config> = {
+	driver: 'd1-http',
+	dbCredentials: {
+		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+		token: process.env.CLOUDFLARE_D1_TOKEN!,
+	},
+}
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema',
@@ -6,5 +23,5 @@ export default defineConfig({
 	strict: true,
 	dialect: 'sqlite',
 	out: './migrations',
-	driver: 'd1-http',
+	...(process.env.LOCAL_DB_PATH ? localConfig : remoteConfig),
 })
---
+++ migrations/0000_wet_zaran.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `names` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text
+);
